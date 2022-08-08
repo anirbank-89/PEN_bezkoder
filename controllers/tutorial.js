54bb725e2c9a89d6743bf8cf25e37da0dd971736
Anirban Karmakar <anirban.k@mantralabs.tech>
2022-08-08 17:45:49 +0530

    delete one, delete all, find all published api

--- controllers/tutorial.js
+++ controllers/tutorial.js
@@ -67,7 +67,7 @@ var findOne = async (req, res) => {
     await TUTORIAL.findByPk(id)
         .then(docs => {
             if (docs == null || typeof docs == "undefined") {
-                res.status(404).send({ 
+                res.status(404).send({
                     message: `Cannot find Tutorial with id=${id}.`
                 });
             }
@@ -89,7 +89,7 @@ var findOne = async (req, res) => {
 }
 
 // Update a Tutorial by the id in the request
-var editTutorial = async (req, res) => { 
+var editTutorial = async (req, res) => {
     // Validate request
     if (!req.body.title || !req.body.description) {
         res.status(400).send({
@@ -128,11 +128,61 @@ var editTutorial = async (req, res) => {
 }
 
 // Delete a Tutorial with the specified id in the request
-var deleteTutorial = async (req, res) => { }
+var deleteTutorial = async (req, res) => {
+    var id = req.params.id;
 
-var deleteAll = async (req, res) => { }
+    TUTORIAL.destroy({ where: { id: id } })
+        .then(docs_num => {
+            if (docs_num == 1) {
+                res.status(200).json({
+                    message: "Data successfully deleted",
+                    documents: docs_num[0]
+                });
+            }
+            else {
+                res.status(404).send({
+                    message: `Data with id ${id} not found.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(501).json({
+                status: false,
+                message: "Invalid id. Server error.",
+                error: err.message
+            });
+        });
+}
 
-var findAllPublished = async (req, res) => { }
+// Delete all Tutorials from the database.
+var deleteAll = async (req, res) => {
+    TUTORIAL.destroy({
+        where: {},
+        truncate: false
+    })
+        .then(nums => {
+            res.send({ message: `${nums} Tutorials were deleted successfully!` });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while removing all tutorials."
+            });
+        });
+}
+
+var findAllPublished = async (req, res) => {
+    TUTORIAL.findAll({ where: { published: true } })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
+}
 
 module.exports = {
     createTutorial,
